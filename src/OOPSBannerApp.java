bf23df28ff16659bdf2eece6f4ab684dc38a0fa3
soumali11 <soumalisen09@gmail.com>
2026-02-23 23:24:50 +0530

    Banner Array initialised

---
+++ src/OOPSBannerApp.java
@@ -0,0 +1,59 @@
+/**
+ * OOPSBannerApp UC6 - OOPS Banner Application (Use Case 6)
+ *
+ * This use case extends UC5 by implementing a modular approach to generate each
+ * letter's pattern through dedicated methods. This enhances code reusability and
+ * maintainability by separating pattern generation logic from the main display logic.
+ *
+ * @author Soumali
+ * @version 6.0
+ */
+
+
+
+
+public class OOPSBannerApp {
+    public static String[] getOPatternO() {
+        return new String[] {
+            "   ***   ",
+            "**     **",
+            "**     **",
+            "**     **",
+            "**     **",
+            "**     **",
+            "   ***   "
+        };
+    }
+    public static String[] getPPattern(){
+        return new String[]{
+            " *****   ",
+            " **  **  ",
+            " **   ** ",
+            " ** **   ",
+            " **      ",
+            " **      ",
+            " **      "
+        };
+    }
+    public static String[] getSPattern(){
+        return new String[]{
+            "   ***   ",
+            " **    **",
+            " **      ",
+            "    **   ",
+            "      ** ",
+            " **   ** ",
+            "   ***   "
+        };
+    }
+
+    public static void main(String[] args){
+        String[] oPattern = getOPatternO();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(oPattern[i] + "   " + oPattern[i] + "   " + pPattern[i] + "   " + sPattern[i]);
+        }
+    }
+}
